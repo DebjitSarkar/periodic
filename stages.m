d959589bb5086d4f5861a96c988a89287622b82d
Debjit Sarkar <debjits11@yahoo.com>
2018-09-24 12:42:20 -0400

    Storing previous iterations
    Extended S-param matrix to store values for all capacitances

--- stages.m
+++ stages.m
@@ -38,12 +38,13 @@ Yall = 1./(1/(1j*w*L0)+1j*w*Call);
 
 %Y_Call = linspace(Y_Cmin, Y_Cmax, T_saw);
 
-toggle = true;
+toggle = true; % Used to initialize phase_min/max
 phase_min = 0; % in degrees
 phase_max = 0;
-phase_vec = zeros(1,T_saw);
+phase_vec = zeros(1,T_saw); % Stores phases for each cap val
+S_vec(:,:,1:T_saw) = zeros(2,2,T_saw); % Stores S-params
 
-%% Computation
+%% Finding phases for single segment
 
 for foo = 1:T_saw
     %Y = Y_Call(foo) + Y_L;
@@ -57,22 +58,30 @@ for foo = 1:T_saw
     
     full_ABCD = TL_ABCD * shunt_ABCD * TL_ABCD; % ABCD for 1 segment
     
-    %full_S = abcd2s(full_ABCD, Z0);
+    %full_S = abcd2s(full_ABCD, Z0); doesn't work for symbolic vars
     A = full_ABCD(1,1);
     B = full_ABCD(1,2);
     C = full_ABCD(2,1);
     D = full_ABCD(2,2);
-    denom = A + B / Z0 + C * Z0 + D;
-    S11 = (A + B / Z0 - C * Z0 - D) / denom;
-    S12 = 2 * (A * D - B * C) / denom;
-    S21 = 2 / denom;
-    S22 = (-A + B / Z0 - C * Z0 + D) / denom;
     
-    phase = rad2deg(wrapTo2Pi(angle(S21)));
+    denom = A + B / Z0 + C * Z0 + D; % ABCD to S
+    S_vec(1,1,foo) = (A + B / Z0 - C * Z0 - D) / denom;
+    S_vec(1,2,foo) = 2 * (A * D - B * C) / denom;
+    S_vec(2,1,foo) = 2 / denom;
+    S_vec(2,2,foo) = (-A + B / Z0 - C * Z0 + D) / denom;
+    %S11 = (A + B / Z0 - C * Z0 - D) / denom;
+    %S12 = 2 * (A * D - B * C) / denom;
+    %S21 = 2 / denom;
+    %S22 = (-A + B / Z0 - C * Z0 + D) / denom;
+    
+    %phase = rad2deg(wrapTo2Pi(angle(S21)));
+    %phase = rad2deg(angle(S21));
+    phase = rad2deg(angle(S_vec(2,1,foo)));
     
     phase_vec(foo) = phase; % Write phase for plotting
     
-    if(toggle) % Update phase min/max
+    % Update phase min/max
+    if(toggle) % Initialize
         phase_min = phase;
         phase_max = phase;
         toggle = false;
@@ -88,13 +97,16 @@ end
 figure;
 hold on;
 scatter(Call, phase_vec);
-title('Phase range vs C');
+title('Phase range vs capacitance for one segment');
 xlabel('C [F]');
 ylabel('Phase [\circ]');
 
 %scatter(imag(Y_Call), phase_vec);
 %title('Phase range vs Y_C');
 %xlabel('Y_C [1/\omega]');
+
+%% Determine minimum number of segments
+
 
 %% Outputs
 % C = 1; % Center capacitance
