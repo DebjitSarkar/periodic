c1389fd509bf3d178b9663f80c15fded28579f83
Debjit Sarkar <sarkard@umich.edu>
2018-09-27 00:59:20 -0400

    Fixed ABCD matrix

--- stages2.m
+++ stages2.m
@@ -17,11 +17,35 @@ w = w0;
 % Check either B or C, or both A and D are wrong (probably C)
 % Check det(ABCD) = 1
 ABCD = [cos(2*BL)+Z0*sin(2*BL)*(1-w^2*L*C)/(2*w*L),... %A
-    1j*Z0*sin(2*BL)+1j*Z0^2*sin(BL)^2*(1-w^2*L*C);...  %B
+    1j*Z0*sin(2*BL)+1j*Z0^2*sin(BL)^2*(1-w^2*L*C)/(w*L);...  %B
     1j*Y0*sin(2*BL)-1j*cos(BL)^2*(1-w^2*L*C)/(w*L),... %C
     cos(2*BL)+Z0*sin(2*BL)*(1-w^2*L*C)/(2*w*L)];       %D
 
 S21 = 2/(ABCD(1,1)+ABCD(1,2)/Z0+ABCD(2,1)*Z0+ABCD(2,2));
+
+%% det(ABCD) = 1 check
+q = subs(det(ABCD), 'C', 1e-15);
+qq = subs(q, 'L', 1e-7);
+vpa(qq)
+
+%%
+%TL_ABCD = [cos(BL), 1j*Z0*sin(BL);
+ %          1j*sin(BL)/Z0, cos(BL)];
+syms a1 b1 c1 d1 a2 b2 c2 d2;
+syms BL Y;
+q1 = [a1 b1; c1 d1];
+q2 = [a2 b2; c2 d2];
+q3 = q1 * q2 * q1;
+q4 = subs(q3, 'a1', cos(BL));
+q4 = subs(q4, 'b1', 1j*Z0*sin(BL));
+q4 = subs(q4, 'c1', 1j*sin(BL)/Z0);
+q4 = subs(q4, 'd1', cos(BL));
+q4 = subs(q4, 'a2', 1);
+q4 = subs(q4, 'b2', 0);
+q4 = subs(q4, 'c2', Y); % c2 = Y
+q4 = subs(q4, 'd2', 1);
+
+q5 = subs(det(q4), 'BL', pi/4); % This works!
 
 %%
 Cinit = 1e-12; % Start
